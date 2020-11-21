9b02c478e41692249c1261e23433afe720da2d52
grechkoaa <grechkoaa@gmail.com>
2020-11-21 23:38:55 +0600

    Fiexed, changed the parameters of the OnLoadScene method in the class LoadScene

--- Assets/Scripts/UI/MainMenu/LoadScene.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class LoadScene : MonoBehaviour
-{
-    public void OnLoadScene(Object scene)
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(scene.name);
-    }
-}
