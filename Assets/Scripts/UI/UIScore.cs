170e3de9d74f6b882ba8d5f60a52ead94f9a78b4
grechkoaa <grechkoaa@gmail.com>
2020-11-21 19:28:52 +0600

    added a panel for game points added game points text added model adding points to class ScoreModel added display of glasses to the screen

---
+++ Assets/Scripts/UI/UIScore.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UIScore : MonoBehaviour
+{
+    [SerializeField] private ScoreModel _scoreModel;
+    [SerializeField] private TMP_Text _text;
+
+    private void OnEnable()
+    {
+        _scoreModel.Encouraged += (points) => _text.text = points.ToString("F0");
+    }
+}
