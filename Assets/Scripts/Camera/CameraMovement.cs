73c9e7c2d48fc5df427423a8f94b1903735e6539
grechkoaa <grechkoaa@gmail.com>
2020-11-23 10:01:32 +0600

    Camera Movement script refactoring

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private GlobalAcceleration _globalAcceleration;
     [SerializeField, Min(0)] private float _speed;
-    [SerializeField] Camera _componentCamera;
+    [SerializeField] Camera _camera;
     [SerializeField] private Vector2 _defaultResolution;
 
     private void Start()
@@ -12,26 +13,32 @@ public class CameraMovement : MonoBehaviour
         SetFieldOfView();
     }
 
-    // Не смог рассчитать постоянную ширину для разных разрешений
-    // Подсмотрено https://gist.github.com/Glavak/ada99b57023db3c941c5caebe42a70c5
+    // Не смог рассчитать постоянную ширину для разных разрешений.
+    // Подсмотрено https://gist.github.com/Glavak/ada99b57023db3c941c5caebe42a70c5.
     private void SetFieldOfView()
     {
         float targetAspect = _defaultResolution.x / _defaultResolution.y;
-        float initialFov = _componentCamera.fieldOfView;
+        float initialFov = _camera.fieldOfView;
         float horizontalFov = CalcVerticalFov(initialFov, 1 / targetAspect);
-        float constantWidthFov = CalcVerticalFov(horizontalFov, _componentCamera.aspect);
-        _componentCamera.fieldOfView = constantWidthFov;
+        float constantWidthFov = CalcVerticalFov(horizontalFov, _camera.aspect);
+
+        _camera.fieldOfView = constantWidthFov;
     }
 
     private float CalcVerticalFov(float hFovInDeg, float aspectRatio)
     {
-        float hFovInRads = hFovInDeg * Mathf.Deg2Rad;
-        float vFovInRads = 2 * Mathf.Atan(Mathf.Tan(hFovInRads / 2) / aspectRatio);
+        var hFovInRads = hFovInDeg * Mathf.Deg2Rad;
+        var vFovInRads = 2 * Mathf.Atan(Mathf.Tan(hFovInRads / 2) / aspectRatio);
 
         return vFovInRads * Mathf.Rad2Deg;
     }
 
     private void Update()
+    {
+        MoveForward();
+    }
+
+    private void MoveForward()
     {
         transform.position += Vector3.forward * _speed * Time.deltaTime;
     }
